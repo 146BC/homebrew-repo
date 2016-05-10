bb87482dca17846bf69c8a8aeccb7f119970b211
Bernard Gatt <bernard@blinkrocket.com>
2016-05-11 01:36:12 +0200

    Added version 0.1.0 to RomeBuild

--- Formula/romebuild.rb
+++ Formula/romebuild.rb
@@ -1,9 +1,9 @@
 class Romebuild < Formula
-  desc "CLI tool for Carthage users to speed up their build times"
+  desc "A CLI tool that uses Carthage dependency resolution and building to deliver pre-built dependencies through Rome Server"
   homepage "https://github.com/146BC/RomeBuild"
-  head "https://github.com/BernardGatt/RomeBuild.git", :branch => "feature/build"
+  head "https://github.com/146BC/RomeBuild.git", :branch => "develop"
   url "https://github.com/146BC/RomeBuild/archive/0.1.0.tar.gz"
-  sha256 ""
+  sha256 "ba3b6da5d583850babf3b9e010f15229685919486b59ce6e3d5dc8347117f46a"
 
   depends_on :xcode => ["7.3", :build]
   depends_on "carthage"
