2202b90fb9bfce88d2a5ce0322e0a5837d38150a
Bernard Gatt <bernard@blinkrocket.com>
2016-05-12 20:03:56 +0200

    Updated RomeBuild with 0.1.2 hot fix.

--- Formula/romebuild.rb
+++ Formula/romebuild.rb
@@ -2,8 +2,8 @@ class Romebuild < Formula
   desc "A CLI tool that uses Carthage dependency resolution and building to deliver pre-built dependencies through Rome Server"
   homepage "https://github.com/146BC/RomeBuild"
   head "https://github.com/146BC/RomeBuild.git", :branch => "develop"
-  url "https://github.com/146BC/RomeBuild/archive/0.1.1.tar.gz"
-  sha256 "de70de2cd4a52ba47c1d2d18f48b2e638950a1e598573b5f60ffb2ba5e4c8dcd"
+  url "https://github.com/146BC/RomeBuild/archive/0.1.2.tar.gz"
+  sha256 "bc78d74563dc8d2ae6ecfcd60b774b230ad972190220576b6360a7529efae071"
 
   depends_on :xcode => ["7.3", :build]
   depends_on "carthage"
