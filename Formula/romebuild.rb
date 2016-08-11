1073932f85dd43f92d6d314a8ff18a9ebe2b9eeb
Bernard Gatt <bernard@blinkrocket.com>
2016-08-11 23:52:39 +0200

    Updated romebuild to 0.3.0

--- Formula/romebuild.rb
+++ Formula/romebuild.rb
@@ -2,8 +2,8 @@ class Romebuild < Formula
   desc "A CLI tool that uses Carthage dependency resolution and building to deliver pre-built dependencies through Rome Server"
   homepage "https://github.com/146BC/RomeBuild"
   head "https://github.com/146BC/RomeBuild.git", :branch => "develop"
-  url "https://github.com/146BC/RomeBuild/archive/0.2.0.tar.gz"
-  sha256 "60e762c46fe0ae3c5a8a30037e21f0d1ba0ddd09ca08088d294213714d3d783a"
+  url "https://github.com/146BC/RomeBuild/archive/0.3.0.tar.gz"
+  sha256 "d55cc013ffd1fdf720dafa9f6749f69d1223879b602d2961bebecb42638c6028"
 
   depends_on :xcode => ["7.3", :build]
   depends_on "carthage"
