d49e8ef096c1075aa1dfe5b29b2676066a06725c
Bernard Gatt <bernard@blinkrocket.com>
2016-09-25 23:26:21 +0200

    Updated Romulus formula to use new romulus branch.

--- Formula/romulus.rb
+++ Formula/romulus.rb
@@ -1,7 +1,7 @@
 class Romulus < Formula
 	desc "A Carthage fork with Rome asset server support"
 	homepage "https://github.com/146BC/Carthage"
-	head "https://github.com/146BC/Carthage.git", :branch => "rome"
+	head "https://github.com/146BC/Carthage.git", :branch => "romulus"
 
 	depends_on :xcode => ["7.3", :build]
 
