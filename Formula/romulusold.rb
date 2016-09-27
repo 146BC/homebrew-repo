476913c6dc5145518a7c94462cbb4e9194c76820
Bernard Gatt <bernard@blinkrocket.com>
2016-09-27 13:55:51 +0200

    Added romulusold formula

---
+++ Formula/romulusold.rb
@@ -0,0 +1,16 @@
+class Romulusold < Formula
+	desc "A Carthage fork with Rome asset server support"
+	homepage "https://github.com/146BC/Carthage"
+	head "https://github.com/146BC/Carthage.git", :branch => "rome"
+
+	depends_on :xcode => ["7.3", :build]
+
+	def install
+		system "make", "prefix_install", "PREFIX=#{prefix}"
+	end
+
+	test do
+		(testpath/"Cartfile").write 'github "jspahrsummers/xcconfigs"'
+		system "#{bin}/carthage", "update"
+	end
+end
