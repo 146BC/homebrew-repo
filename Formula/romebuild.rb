9f23679d78c8192ba9d9699aa0fb3cc03997d4e1
Bernard Gatt <bernard@blinkrocket.com>
2016-10-18 20:55:01 +0200

    Updated RomeBuild to 0.4.0 - Xcode 8 support.

--- Formula/romebuild.rb
+++ Formula/romebuild.rb
@@ -2,10 +2,10 @@ class Romebuild < Formula
   desc "A CLI tool that uses Carthage dependency resolution and building to deliver pre-built dependencies through Rome Server"
   homepage "https://github.com/146BC/RomeBuild"
   head "https://github.com/146BC/RomeBuild.git", :branch => "develop"
-  url "https://github.com/146BC/RomeBuild/archive/0.3.0.tar.gz"
-  sha256 "d55cc013ffd1fdf720dafa9f6749f69d1223879b602d2961bebecb42638c6028"
+  url "https://github.com/146BC/RomeBuild/archive/0.4.0.tar.gz"
+  sha256 "924fa4f11304a24466a67286b448cb454fd6bc8f4d6123f1d8631a5b01d9f738"
 
-  depends_on :xcode => ["7.3", :build]
+  depends_on :xcode => ["8.0", :build]
   depends_on "carthage"
 
   def install
