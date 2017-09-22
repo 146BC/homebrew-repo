1b5cb77b58e2dac7bfed7cbe40c45c42eaa3c1f6
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2017-09-22 04:06:32 +0300

    Fixed install params

--- Formula/romebuild.rb
+++ Formula/romebuild.rb
@@ -2,14 +2,14 @@ class Romebuild < Formula
   desc "A CLI tool that uses Carthage dependency resolution and building to deliver pre-built dependencies through Rome Server"
   homepage "https://github.com/ypopovych/RomeBuild"
   head "https://github.com/ypopovych/RomeBuild.git", :branch => "develop"
-  url "https://github.com/ypopovych/RomeBuild/archive/0.5.0.tar.gz"
-  sha256 "ba93e74a389a41ceca8d2bec7afe3910909a3644e47b1313db4c5e620e39d4fc"
+  url "https://github.com/ypopovych/RomeBuild/archive/0.5.1.tar.gz"
+  sha256 "4068d6563a3a54f9d4a45c051c628d99108fd956219c82ac3e580a48027a3dc3"
 
   depends_on :xcode => ["9.0", :build]
   depends_on "carthage"
 
   def install
-    system "make", "install", "PREFIX=#{prefix}"
+      system "make", "install", "PREFIX=#{prefix}", "BUILDPATH=#{buildpath}"
   end
 
   test do
