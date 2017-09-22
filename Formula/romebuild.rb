23b8fd8f0c9c7a00324c2b1cd34a16eddb6cf911
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2017-09-22 03:57:40 +0300

    Updated RomeBuild version

--- Formula/romebuild.rb
+++ Formula/romebuild.rb
@@ -1,11 +1,11 @@
 class Romebuild < Formula
   desc "A CLI tool that uses Carthage dependency resolution and building to deliver pre-built dependencies through Rome Server"
-  homepage "https://github.com/146BC/RomeBuild"
-  head "https://github.com/146BC/RomeBuild.git", :branch => "develop"
-  url "https://github.com/146BC/RomeBuild/archive/0.4.0.tar.gz"
-  sha256 "924fa4f11304a24466a67286b448cb454fd6bc8f4d6123f1d8631a5b01d9f738"
+  homepage "https://github.com/ypopovych/RomeBuild"
+  head "https://github.com/ypopovych/RomeBuild.git", :branch => "develop"
+  url "https://github.com/ypopovych/RomeBuild/archive/0.5.0.tar.gz"
+  sha256 "ba93e74a389a41ceca8d2bec7afe3910909a3644e47b1313db4c5e620e39d4fc"
 
-  depends_on :xcode => ["8.0", :build]
+  depends_on :xcode => ["9.0", :build]
   depends_on "carthage"
 
   def install
